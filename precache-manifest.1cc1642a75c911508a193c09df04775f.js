084a786b8b0f400492cc0cf31d89e4bb762214fa
aaronhleung <aaronleung@outlook.com>
2022-06-04 11:12:22 -0700

    Updates

---
+++ precache-manifest.1cc1642a75c911508a193c09df04775f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ce3bcb1343cd96ed1715d34e2affadb",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "f6378dd436b27e7c8950",
+    "url": "/react-portfolio/static/css/main.be24e41c.chunk.css"
+  },
+  {
+    "revision": "ec7dea27f2d267470dff",
+    "url": "/react-portfolio/static/js/2.102548e6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-portfolio/static/js/2.102548e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6378dd436b27e7c8950",
+    "url": "/react-portfolio/static/js/main.9ef326fc.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "8f40166b6d46c2dbda78ce0194f9f7b5",
+    "url": "/react-portfolio/static/media/budget-tracker.8f40166b.jpg"
+  },
+  {
+    "revision": "938f8fe0fe2e5880b3ef528c5188a096",
+    "url": "/react-portfolio/static/media/calculator.938f8fe0.jpg"
+  },
+  {
+    "revision": "1bdce90762b1008c7d4a7aa1a8fa3aff",
+    "url": "/react-portfolio/static/media/coding-quiz.1bdce907.jpg"
+  },
+  {
+    "revision": "71b72d4e62f2e0b0394cd4ba6b71a826",
+    "url": "/react-portfolio/static/media/cover-image.71b72d4e.jpg"
+  },
+  {
+    "revision": "27b03b4bef0184fbdf678abb1e6b0b05",
+    "url": "/react-portfolio/static/media/dread-it.27b03b4b.jpg"
+  },
+  {
+    "revision": "ecc4495eb6739f919822aa9e7407a748",
+    "url": "/react-portfolio/static/media/e-commerce.ecc4495e.jpg"
+  },
+  {
+    "revision": "ceb9dc3e3d1edabff073c7396133ca3b",
+    "url": "/react-portfolio/static/media/inspo.ceb9dc3e.jpg"
+  },
+  {
+    "revision": "555b76bf7eedeb1fbdec66cfa92d79cc",
+    "url": "/react-portfolio/static/media/note-taker.555b76bf.jpg"
+  },
+  {
+    "revision": "cfbb297201b42ecddda1cf9b329f6d6b",
+    "url": "/react-portfolio/static/media/ourspace.cfbb2972.jpg"
+  },
+  {
+    "revision": "03ae8eaa5757b9a425623e6203d57347",
+    "url": "/react-portfolio/static/media/tech-blog.03ae8eaa.jpg"
+  }
+]);
